2a8a5ab46ff87f2fb707cd22fe2ba2a0eac3aa53
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2017-01-01 14:07:05 +0100

    Use arguments from command line

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,10 @@ fn main() {
     let gitignore_path = pwd.join(".gitignore");
     let gitignore_file = gitignore::File::new(&gitignore_path).unwrap();
 
+    let args: Vec<_> = env::args().collect();
+    let ref to_replace = args[1];
+    let ref replace_with = args[2];
+
     for path in glob("**/*").unwrap()
         .filter_map(Result::ok)
         .map(|p| pwd.join(&p))
@@ -45,7 +49,7 @@ fn main() {
                 drop(file);  // Close the file early
 
                 // Run replace operation in memory
-                let new_data = s.replace("bar", "bar");
+                let new_data = s.replace(to_replace, replace_with);
 
                 // Recreate the file and dump the processed contents to it
                 let mut f = File::create(&path).expect("Unable to create file");
