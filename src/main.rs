4e6d79ab6e67a37910d2db386057d28e1fc7a4cf
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2017-01-01 13:43:57 +0100

    Move all code into loop

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::env;
+use std::fs::metadata;
 
 extern crate glob;
 extern crate gitignore;
@@ -21,25 +22,26 @@ fn main() {
     for path in glob("**/*").unwrap()
         .filter_map(Result::ok)
         .map(|p| pwd.join(&p))
+        .filter(|p| metadata(&p).unwrap().is_file())
         .filter(|p| !gitignore_file.is_excluded(&p).unwrap()) {
         println!("{}", path.display());
-    }
 
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let mut file = match File::open(&path) {
-        // The `description` method of `io::Error` returns a string that
-        // describes the error
-        Err(why) => panic!("couldn't open {}: {}", display,
-                                                   why.description()),
-        Ok(file) => file,
-    };
+        // Open the path in read-only mode, returns `io::Result<File>`
+        let mut file = match File::open(&path) {
+            // The `description` method of `io::Error` returns a string that
+            // describes the error
+            Err(why) => panic!("couldn't open {}: {}", display,
+                                                       why.description()),
+            Ok(file) => file,
+        };
 
-    // Read the file contents into a string, returns `io::Result<usize>`
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("couldn't read {}: {}", display,
-                                                   why.description()),
-        Ok(_) => print!("{} contains:\n{}", display, s.replace("bar", "quox")),
+        // Read the file contents into a string, returns `io::Result<usize>`
+        let mut s = String::new();
+        match file.read_to_string(&mut s) {
+            Err(why) => panic!("couldn't read {}: {}", display,
+                                                       why.description()),
+            Ok(_) => print!("{} contains:\n{}", display, s.replace("bar", "quox")),
+        }
     }
 
     // `file` goes out of scope, and the "hello.txt" file gets closed
